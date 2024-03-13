fbdbf252073db78d399c873bc7df6cbd50a71aa4
MICHAELKITH <michaelkithinji27@gmail.com>
2024-03-13 16:18:23 +0300

    Add first commit

---
+++ wizard/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h1 className='text-blue-500 text-4xl'>Hello Pro mike</h1>
+    </>
+  )
+}
+
+export default App
