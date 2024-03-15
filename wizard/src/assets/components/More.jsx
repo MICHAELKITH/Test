142c5bc8dee9d52e8e84bfe2c345b9475f162a88
MICHAELKITH <michaelkithinji27@gmail.com>
2024-03-15 13:15:24 +0300

    Add changes for more components

--- wizard/src/assets/components/More.jsx
+++ wizard/src/assets/components/More.jsx
@@ -4,11 +4,11 @@ const More = () => {
   return (
     <div className="flex justify-center items-center w-full relative">
         
-      <div className="px-10 md:ml-10 bg-gradient-to-r from-gray-900 via-amber-700 to-slate-900 text-white rounded-lg md:w-3/4 gap-80 md:flex w-full">
+      <div className="px-10 md:ml-10 bg-gradient-to-r from-gray-900 via-amber-700 to-slate-900 text-white rounded-lg md:w-3/4 w-5/6 gap-80 md:flex ">
         
         <div className="md:py-8 text-white gap-4 py-2 md:px-8">
-          <h1 className="text-2xl ">Create Tutorials in 3 Clicks.</h1>
-          <p className="">
+          <h1 className="md:text-2xl ">Create Tutorials in 3 Clicks.</h1>
+          <p className="text-sm py-2">
             With WizardShot, You Click Capture, And We’ll Capture What’s
             Happening on <br /> Your Screen And Turn It Into A Tutorial.
           </p>
